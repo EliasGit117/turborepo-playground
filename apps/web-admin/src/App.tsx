14143e8fba82f22ebfdd0d7af27f6fbc92e4e995
eliassmith <thewillthatishiddeninside@gmail.com>
2024-05-28 16:29:11 +0300

    Added react vite

---
+++ apps/web-admin/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@repo/ui/button';
+
+
+function App() {
+
+  return (
+    <main>
+
+      <Button appName="admin">
+        Click me!
+      </Button>
+
+    </main>
+  )
+}
+
+export default App
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,6 +11,13 @@ interface ButtonProps {
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
+      style={{
+        backgroundColor: 'black',
+        padding: 8,
+        color: 'white',
+        borderRadius: 4,
+        border: 'none'
+      }}
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
