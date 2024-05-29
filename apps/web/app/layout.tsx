0a47d42ea0478e54e0915f931ea1003a207bbe5a
eliassmith <thewillthatishiddeninside@gmail.com>
2024-05-29 20:17:15 +0300

    Added ShadCN theme

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,33 @@
-import "@repo/ui/globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import '@ui/globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '../components/theme-provider';
+import { ReactNode } from 'react';
+import ThemeDropdown from '../components/theme-dropdown';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Turborepo Playground",
+  title: 'Turborepo Playground'
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <header className="container mx-auto p-4 flex justify-between">
+        <h1 className="text-3xl">Web App for clients</h1>
+        <ThemeDropdown/>
+      </header>
+      {children}
+    </ThemeProvider>
+    </body>
     </html>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@repo/ui/components/ui/button';
+import { Button } from '@ui/components/ui/button';
 
 
 export default function Page() {
   return (
-    <main className="container mx-auto py-4">
-
+    <main className="container mx-auto py-4 space-y-4">
       <Button>
         Click me!
       </Button>
